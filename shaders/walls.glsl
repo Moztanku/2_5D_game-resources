3231820eab604e301e445b7bceb40e662090cbab
Moztanku <47280591+Moztanku@users.noreply.github.com>
2024-01-10 17:18:58 +0100

    Update walls.glsl

--- shaders/walls.glsl
+++ shaders/walls.glsl
@@ -4,11 +4,25 @@
 layout(location = 0) in vec2 position;
 
 uniform mat4 u_projection;
+uniform vec2 u_PlayerPos;
+uniform float u_PlayerRot;
+
+vec2 rotate(vec2 ver, vec2 origin, float angle)
+{
+    float s = sin(angle);
+    float c = cos(angle);
+
+    ver -= origin;
+    ver = vec2(ver.x * c - ver.y * s, ver.x * s + ver.y * c);
+    ver += origin;
+
+    return ver;
+}
 
 void main()
 {
-
-    gl_Position = u_projection * vec4(position, 0.0, 1.0);
+    vec2 pos = rotate(position, u_PlayerPos, -u_PlayerRot + 3.1415926535897932384626433832795 / 2.0);
+    gl_Position = u_projection * vec4(pos - u_PlayerPos, 0.0, 1.0);
 }
 
 #shader fragment
